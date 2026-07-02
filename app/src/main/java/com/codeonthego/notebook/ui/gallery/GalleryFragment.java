16d73c2401613c689396a907306fc566433b074b
Mark Rose <mark.rose@nasa.gov>
2026-07-02 16:24:10 -0700

    fix imports

--- app/src/main/java/com/codeonthego/notebook/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/codeonthego/notebook/ui/gallery/GalleryFragment.java
@@ -2,6 +2,7 @@ package com.codeonthego.notebook.ui.gallery;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;

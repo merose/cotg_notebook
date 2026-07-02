2ced7217cf9932909a2a6cbef868dce7643c3e97
Mark Rose <mark.rose@nasa.gov>
2026-07-02 15:34:39 -0700

    Add editable text to gallery fragment

--- app/src/main/java/com/codeonthego/notebook/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/codeonthego/notebook/ui/gallery/GalleryFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,24 +16,36 @@ import com.codeonthego.notebook.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends Fragment {
 
-    private FragmentGalleryBinding binding;
+//    private FragmentGalleryBinding binding;
+    private LinearLayout view;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         GalleryViewModel galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
 
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+//        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+//        View root = binding.getRoot();
+//
+//        final TextView textView = binding.textGallery;
+//        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        return root;
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        view = new LinearLayout(this);
+        view.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        );
+        view.setOrientation(LinearLayout.LayoutParams.VERTICAL);
+
+        var editText = new EditText(this);
+        editText.setText("hello, world", TextView.BufferType.EDITABLE);
+        view.addView(editText);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+//        binding = null;
     }
 }
